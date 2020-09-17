81c75bb192993e1b06f19c14c993ae1ba97e6f3a
Thorsten Ball <mrnugget@gmail.com>
2020-09-17 10:52:41 +0200

    Add main.go file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sourcegraph-testing/tiny-go-test-repository
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+const basePath = "/tmp"
+
+func loadFile(name string) (*os.File, error) {
+	fpath := filepath.Join(basePath, name)
+
+	f, err := os.Open(fpath)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("could not open file %q", name))
+	}
+
+	return f, nil
+}
+
+func main() {
+	for i := 0; i < 5; i++ {
+		f, err := loadFile(fmt.Sprintf("%d", i))
+		if err != nil {
+			fmt.Printf("error: %s\n", err)
+			os.Exit(1)
+		}
+
+		defer func() {
+			if err := f.Close(); err != nil {
+				fmt.Printf("error: %s\n", err)
+				os.Exit(1)
+			}
+		}()
+
+		f.WriteString(fmt.Sprintf("Hello in file %d!\n", i))
+	}
+}
